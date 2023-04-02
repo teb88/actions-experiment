d6624d2236727127ff59e05ed7496db1ad12c620
Esteban Gomez <estebanrc88@gmail.com>
2023-04-02 19:47:27 -0300

    add a test that fails

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("this one should fail", () => {
+  const linkElement = screen.getByText(/does not exist/i);
   expect(linkElement).toBeInTheDocument();
 });
